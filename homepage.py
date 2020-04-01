df5fa39681548272343d8c4f0fb52a8d5ee67724
Galit <glukin@mit.edu>
2020-04-01 10:42:55 -0400

    us map in projections tab

--- homepage.py
+++ homepage.py
@@ -10,54 +10,14 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 from navbar import Navbar
-from projections import df_projections
+from projections import build_us_map
 from assets.colMapping import states
 
 nav = Navbar()
 
-def build_us_map():
-
-    global df_projections
+def build_tom_us_map():
     tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-    df_map = df_projections.loc[df_projections['Day']==tomorrow]
-    df_map = df_map.applymap(str)
-
-    df_map.loc[:,'code'] = df_map.State.apply(lambda x: states[x])
-
-    fig = go.Figure()
-
-    df_map.loc[:,'text'] = df_map['State'] + '<br>' + \
-                'Total Detected ' + df_map['Total Detected'] + '<br>' + \
-                'Current Active ' + df_map['Current Active'] + '<br>' + \
-                'Current Hospitalized ' + df_map['Current Hospitalized'] + '<br>' + \
-                'Total Hospitalized ' + df_map['Total Hospitalized'] + '<br>' + \
-                'Total Detected Deaths ' + df_map['Total Detected Deaths']
-
-    fig = go.Figure(data=go.Choropleth(
-            locations=df_map['code'],
-            z=df_map['Current Active'].astype(float),
-            locationmode='USA-states',
-            colorscale='Reds',
-            autocolorscale=False,
-            text=df_map['text'], # hover text
-            marker_line_color='white', # line markers between states
-            colorbar_title='Tomorrow\'s Predictioned Active Counts'
-        ))
-
-    fig.update_layout(
-            title_text='Tomorrow\'s Predictions of COVID-19 Cases',
-            geo = dict(
-                scope='usa',
-                projection=go.layout.geo.Projection(type = 'albers usa'),
-                showlakes=True, # lakes
-                lakecolor='rgb(255, 255, 255)'),
-        )
-
-    graph = dcc.Graph(
-        id='projection-map',
-        figure=fig
-    )
-    return graph
+    return build_us_map(tomorrow)
 
 body = dbc.Container(
     [
@@ -76,13 +36,13 @@ body = dbc.Container(
                          """
                            )
                    ],
-                  width=3
+                  width=3.5
                ),
                     dbc.Col(
                       [
                           html.Div(
-                          id = 'us_map',
-                          children = build_us_map(),
+                          id = 'us_map_homepage',
+                          children = build_tom_us_map(),
                           ),
                       ]
                       )
