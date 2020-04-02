76be6a7705a8cf5eb0b8229a7ee6b53015200ae3
Galit <glukin@mit.edu>
2020-04-02 10:30:51 -0400

    fixing indentation on homepage

--- homepage.py
+++ homepage.py
@@ -20,33 +20,35 @@ def build_tom_us_map():
     return build_us_map(tomorrow)
 
 body = dbc.Container(
-    dbc.Row(
     [
-        dbc.Col(
+        dbc.Row(
         [
-            html.H1("COVID-19"),
-            html.H2("Predictive Tools"),
-            html.P(
-                 """\
-                 MIT’s Operations Research Center provides open source interactive tools and dataset to predict patients outcome probabilities, including their potential Intensive Care Unit needs and expected length of stay in hospital.
+            dbc.Col(
+            [
+                html.H1("COVID-19"),
+                html.H2("Predictive Tools"),
+                html.P(
+                     """\
+                     MIT’s Operations Research Center provides open source interactive tools and dataset to predict patients outcome probabilities, including their potential Intensive Care Unit needs and expected length of stay in hospital.
 
-                 We aim to rapidly develop, vet, and deliver tools for use by hospitals in the United States to combat the spread of COVID-19.
+                     We aim to rapidly develop, vet, and deliver tools for use by hospitals in the United States to combat the spread of COVID-19.
 
-                 """
+                     """
+                )
+            ],
+            width=3.5
+            ),
+            dbc.Col(
+            [
+              html.Div(
+                  id = 'us_map_homepage',
+                  children = build_tom_us_map(),
+              ),
+            ]
             )
         ],
-        width=3.5
-        ),
-        dbc.Col(
-        [
-          html.Div(
-              id = 'us_map_homepage',
-              children = build_tom_us_map(),
-          ),
-        ]
         )
     ],
-    ),
     className="hompage-body",
 )
 
