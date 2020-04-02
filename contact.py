a4bb65b5283ad244878c099b72e76d66b3e90ce4
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2020-04-02 16:45:34 -0400

    Working contact form + ML hyperlink

--- contact.py
+++ contact.py
@@ -13,67 +13,81 @@ from dash.dependencies import Output, Input
 from navbar import Navbar
 import yaml
 
+
+# Form details
+formspree_url="https://formspree.io/xqkdkggg"
+mailing_list="covidanalytics@mit.edu"
+
+
+
 nav = Navbar()
+name_input = dbc.FormGroup(
+    [
+        dbc.Col(html.Label("Name", htmlFor="name-row"), width=1),
+        dbc.Col(
+            dcc.Input(
+                type="text",
+                id="name-row",
+                name="name",
+                className="form-control",
+                placeholder="Enter the topic of your message",
+            ),
+            width=6,
+        ),
+    ],
+    row=True,
+)
 
 email_input = dbc.FormGroup(
     [
-        dbc.Label("Email", html_for="email-row", width=2),
+        dbc.Col(html.Label("Email", htmlFor="email-row"), width=1),
         dbc.Col(
-            dbc.Input(
-                type="email", id="email-row", placeholder="Enter your email address"
-            ),
-            width=10,
+            [dcc.Input(
+                type="email",
+                id="email-row",
+                name="email",
+                className="form-control",
+                placeholder="Enter your email address"),
+            dcc.Input(
+                type="hidden",
+                name="_subject",
+                value="Covidanalytics Mailing List")]
+            ,
+            width=6,
         ),
     ],
     row=True,
 )
 
-topic_input = dbc.FormGroup(
-    [
-        dbc.Label("Name", html_for="topic-row", width=2),
-        dbc.Col(
-            dbc.Input(
-                type="email",
-                id="topic-row",
-                placeholder="Enter the topic of your message",
-            ),
-            width=10,
-        ),
-    ],
-    row=True,
-)
 
 text_input = dbc.FormGroup(
     [
-        dbc.Label("Text", html_for="message-row", width=2),
-        dbc.Col([
-            dbc.Textarea(placeholder="Your message goes here...", 
-                         ),
-            dbc.FormText("Type something in the box above")],
-            width=10,
-          ),
-        
+        dbc.Label("Text", html_for="message-row", width=1),
+        dbc.Col([ dcc.Textarea(placeholder="Your message goes here...",
+            className="form-control",
+            id="message-row",
+            name="message")],
+            width=6),
     ],
     row=True,
 )
 
-submit_button = dbc.Button("Submit", color="primary")
+submit_button = html.Button("Submit", className="btn btn-primary", formEncType="submit")
 
-form = dbc.Form([email_input, topic_input, text_input, submit_button])
+form = html.Form([name_input, email_input, text_input, submit_button],
+                 action=formspree_url,
+                 method="POST")
 
 body = dbc.Container([dbc.Row([dbc.Col([html.H1("COVID-19"),
-                                        html.H5("We are happy to collaborate and help you take our research one step further."),
-                                        html.H5("Feel free to send us an email using that the following form."),
-                                        html.H5("You can also reach out to us by sending an email to covidanalytics@mit.edu."),
+                                        html.P("We are happy to collaborate and help you take our research one step further." +
+                                               "Feel free to send us an email using that the following form."),
+                                        html.P(["You can also reach out to us by sending an email to ", html.A(
+                                            mailing_list, href="mailto:%s" % mailing_list, ), "."]),
                                         form
-                                         ])])])
+                                        ])])])
 
 def Contact():
-    layout = html.Div(
-    [
-        nav,
-        body,
-    ])
+    layout = html.Div( [nav, body])
     return layout
 
 app = dash.Dash(__name__, external_stylesheets = [dbc.themes.UNITED])
