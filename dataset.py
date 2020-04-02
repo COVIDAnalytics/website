d9cc84438f5a592c998c21af2324389febe94464
Agni <agniorf@mit.edu>
2020-04-02 15:02:54 -0400

    add dataset text

--- dataset.py
+++ dataset.py
@@ -68,7 +68,9 @@ body = dbc.Container([
 	dbc.Row(
            [
                dbc.Col(
-                  [ dbc.Row( html.H6('In the fog of war of the Covid-19 pandemic, a critical factor inhibiting effective decision making at regional, national, and global levels is a lack of relevant data on patient outcomes. We hope to partially alleviate this problem by sharing the following dataset, which aggregates data from over 100 published clinical studies and preprints released between December 2019 and March 2020.'),),
+                  [ dbc.Row( html.Div('In the fog of war of the Covid-19 pandemic, a critical factor inhibiting effective decision making at regional, national, and global levels is a lack of relevant data on patient outcomes. We hope to partially alleviate this problem by sharing the following dataset, which aggregates data from over 100 published clinical studies and preprints released between December 2019 and March 2020.'),),
+                  	dbc.Row( html.Div('We would like to remind the reader that the raw data in this dataset should not be used to estimate trends in the general population such as mortality rates. Indeed, this dataset is largely derived from studies run in hospitals and nations affected with SARS-COV-2 generally only admit seriously affected patients to hospitals. However, it should be possible to derive reasonably accurate estimates of these quantities by (a) accounting for the prevalence of asymptomatic patients, and (b) only including sufficiently representative studies.'),),
+                  	dbc.Row( html.Div('At a high level, each row of the dataset represents a cohort of patients. Some papers study a single cohort, while others study several cohorts, and still others report results about one cohort and one or more subcohorts; all of these are included as rows in the dataset.'),),
                   ],md=4
                ),
               dbc.Col(
