0bfa057704d2e1934c6973457dae4b01656e4fa4
Galit <glukin@mit.edu>
2020-04-02 10:29:14 -0400

    fixing indentation on homepage

--- homepage.py
+++ homepage.py
@@ -20,41 +20,36 @@ def build_tom_us_map():
     return build_us_map(tomorrow)
 
 body = dbc.Container(
+    dbc.Row(
     [
-       dbc.Row(
-           [
-               dbc.Col(
-                  [
-                     html.H1("COVID-19"),
-                     html.H2("Predictive Tools"),
-                     html.P(
-                         """\
-                         MIT’s Operations Research Center provides open source interactive tools and dataset to predict patients outcome probabilities, including their potential Intensive Care Unit needs and expected length of stay in hospital.
+        dbc.Col(
+        [
+            html.H1("COVID-19"),
+            html.H2("Predictive Tools"),
+            html.P(
+                 """\
+                 MIT’s Operations Research Center provides open source interactive tools and dataset to predict patients outcome probabilities, including their potential Intensive Care Unit needs and expected length of stay in hospital.
 
-                         We aim to rapidly develop, vet, and deliver tools for use by hospitals in the United States to combat the spread of COVID-19.
+                 We aim to rapidly develop, vet, and deliver tools for use by hospitals in the United States to combat the spread of COVID-19.
 
-                         """
-                           )
-                   ],
-                  width=3.5
-               ),
-                    dbc.Col(
-                      [
-                          html.Div(
-                          id = 'us_map_homepage',
-                          children = build_tom_us_map(),
-                          ),
-                      ]
-                      )
-                 ],
+                 """
             )
-       ],
-className="mt-4",
+        ],
+        width=3.5
+        ),
+        dbc.Col(
+        [
+          html.Div(
+              id = 'us_map_homepage',
+              children = build_tom_us_map(),
+          ),
+        ]
+        )
+    ],
+    ),
+    className="hompage-body",
 )
 
 def Homepage():
-    layout = html.Div([
-                    nav,
-                    body
-                    ])
+    layout = html.Div([nav, body])
     return layout
