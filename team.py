3945e7b07c6384b062f451bea560aa5cd58f0b7d
Galit <glukin@mit.edu>
2020-04-05 12:30:17 -0400

    alligning indexing for teams

--- team.py
+++ team.py
@@ -26,22 +26,56 @@ members_per_row = 4
 
 # Single member pic
 def member_pic(member):
-    return dbc.Col([html.Div([html.Img(src='assets/team_members/photos/%s' % member['photo']),
-                              dbc.Button(member['name'], color="info", block=True,
-                                         className="mb-3", href=member['website'])],
-                                         style={'display': 'inline-block'})],
-                   width=3)
+    return dbc.Col(
+        [
+            html.Div(
+            [
+                html.Img(src='assets/team_members/photos/%s' % member['photo']),
+                dbc.Button(
+                    member['name'],
+                    color="info",
+                    block=True,
+                    className="mb-3",
+                    href=member['website'])
+             ],
+             style={'display': 'inline-block'}
+            )
+        ],
+        width=3
+    )
 
 # Table rows
-member_rows = [dbc.Row([member_pic(m) for m in members[i*members_per_row:(i+1)*members_per_row]])
-               for i in range(num_members//members_per_row)] + \
-              [dbc.Row([member_pic(m) for m in members[num_members - (num_members % members_per_row):]])]
+member_rows = [
+    dbc.Row(
+        [
+            member_pic(m) for m in members[i*members_per_row:(i+1)*members_per_row]
+        ]
+    ) for i in range(num_members//members_per_row) +
+    dbc.Row(
+        [
+            member_pic(m) for m in members[num_members - (num_members % members_per_row):]
+        ]
+    )
+    ]
 
 
-body = dbc.Container([dbc.Row([dbc.Col([
-                                        html.H2("Our Team"),
-                                        html.P('Our team comprises passionate researchers in Operations Research and Analytics. We were eager to use our collective skills and create new tools that can help the scientific community fight against the pandemic.')])])] +
-                     member_rows,className="page-body")
+body = dbc.Container(
+        [
+            dbc.Row(
+                [
+                    dbc.Col(
+                    [
+                        html.H2("Our Team"),
+                        html.P('Our team comprises passionate researchers in Operations Research \
+                        and Analytics. We were eager to use our collective skills and create new \
+                        tools that can help the scientific community fight against the pandemic.')
+                    ]
+                    )
+                ]
+            )
+        ] + member_rows,
+        className="page-body"
+    )
 
 def Team():
     layout = html.Div([nav, body, footer])
