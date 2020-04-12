eccd348a85c68b416d78f498d2caf69e7b67875a
Galit <glukin@mit.edu>
2020-04-12 16:26:01 -0400

    fix visibility of wide dropdowns in all size screens

--- projections/projections.py
+++ projections/projections.py
@@ -172,16 +172,18 @@ body = dbc.Container(
                                 dcc.Markdown("What value would you like to know?"),
                                 dbc.Row(
                                     [
-                                        dbc.Col(dcc.Markdown("**Predicted Value:**")),
+                                        dbc.Col(dcc.Markdown("**Predicted  \n Value:**"),width="auto"),
                                         dbc.Col(
                                             html.Div(
                                                 dcc.Dropdown(
                                                     id = 'predicted_timeline',
                                                     options = [{'label': x, 'value': x} for x in cols],
                                                     value = 'Total Detected',
+                                                    style={'width':'100%'}
                                                 ),
                                                 id = "p2-transfer-dropdown-wrapper",
                                             ),
+                                        width=True
                                         ),
                                     ]
                                 ),
