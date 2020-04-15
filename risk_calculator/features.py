95a23307fcb91ede731b2a6bd42bf59e5815e5dd
Galit <glukin@mit.edu>
2020-04-15 14:17:23 -0400

    removing unecessary imports

--- risk_calculator/features.py
+++ risk_calculator/features.py
@@ -1,11 +1,5 @@
-### Data
-import datetime
-import pandas as pd
 import json
-### Graphing
-import plotly.graph_objects as go
-import dash
-import dash_table
+
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
