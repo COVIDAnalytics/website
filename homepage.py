222a711928b7a0205896241ad1c9e914dde20bdc
Galit <glukin@mit.edu>
2020-05-01 16:51:30 -0400

    homepage changes

--- homepage.py
+++ homepage.py
@@ -67,7 +67,7 @@ def build_card(imgTop,title,text,img,link):
                     xs=12,
                     sm=5,
                     md=5,
-                    lg=3,
+                    lg=2,
                 )
 
 
@@ -114,12 +114,11 @@ body = dbc.Container(
         ),
         dbc.Row(
             [
+                build_card(False,"Data and insights",data_text + insights_text,"assets/images/insights-4.png","/interactive-graph"),
                 build_card(True,"Case predictions",projections_text,"assets/images/forecast-1.png","/projections"),
-                build_card(False,"Infection risk calculator",infection_calculator_text,"assets/images/infection.png","/infection_calculator"),
-                build_card(True,"Mortality risk calculator",mortality_calculator_text,"assets/images/mortality.png","/mortality_calculator"),
-                build_card(True,"Ventilator allocation",ventilator_text,"assets/images/allocation.png","/ventilator_allocation"),
-                build_card(False,"Data",data_text,"assets/images/data-1.jpg","/dataset"),
-                build_card(True,"Insights",insights_text,"assets/images/insights-4.png","/interactive-graph"),
+                build_card(False,"Ventilator allocation",ventilator_text,"assets/images/allocation.png","/ventilator_allocation"),
+                build_card(True,"Infection risk calculator",infection_calculator_text,"assets/images/infection_logo.jpg","/infection_calculator"),
+                build_card(False,"Mortality risk calculator",mortality_calculator_text,"assets/images/mortality_logo.png","/mortality_calculator"),
             ],
             justify="around",
             no_gutters=True,
