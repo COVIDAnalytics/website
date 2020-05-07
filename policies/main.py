f037238268d2e52340c2bafa6a7b73a55c788208
Galit <glukin@mit.edu>
2020-05-07 09:12:32 -0400

    improve lag

--- policies/main.py
+++ policies/main.py
@@ -94,8 +94,8 @@ def build_policy_projections(state, policies, times, value):
 
     for p,policy in enumerate(policies):
         if sum(policy) > 0:
-            y = list(range(0,max_y+1,100000))
-            x_vertical = [get_start(times[p])] * len(y)
+            y = [0,max_y]
+            x_vertical = [get_start(times[p])] * 2
             fig.add_trace(go.Scatter(
                 showlegend=False,
                 x=x_vertical,
