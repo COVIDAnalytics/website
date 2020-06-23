d99f177b4cbd5544ae6c77d3a54a2aa28f2ac541
Travis Ziegler <travisjayday@gmail.com>
2020-06-23 16:00:07 -0500

    Cleaned up calculator code in general. Added me as team member.

--- risk_calculator/visuals.py
+++ risk_calculator/visuals.py
@@ -1,225 +1,199 @@
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-
-def get_labs_indicator(id):
-    return  [
-                dbc.Row(
-                [
-                    dbc.Col(
-                    [
-                        dbc.Card(
-                            [
-                                dbc.CardBody(
-                                    [
-                                        dcc.Markdown(id=id+"_text"),
-                                        dbc.Row(
-                                            [
-                                                dbc.Col(
-                                                    html.Div(
-                                                        [
-                                                        dcc.Dropdown(
-                                                                id = id,
-                                                                value = 0,
-                                                        ),
-
-                                                    ]),
-                                                ),
-                                            ]
-                                        ),
-                                    ],
-                                ),
-                            ],
-                            className="projections-general-card h-100"
-                        ),
-                    ],
-                    xs=12,
-                    sm=12,
-                    md=6,
-                    lg=6,
-                    ),
-                ],
-                justify="center",
-                style={"paddingBottom":20}
-                ),
-            ]
-
-def get_model_desc(id):
-    return [
-                dbc.Row(
-                [
-                    dbc.Jumbotron(
-                            id = id,
-                            style={'paddingBottom':'0.5rem','paddingTop':'0.8rem'}
-                    ),
-                ],
-                justify="center"
-                ),
-            ]
-
-def get_feature_importance(id):
-    return [
-                dbc.Row(
-                    dbc.Col(
-                        [
-                            dbc.Card(
-                                id = id,
-                                style={"borderColor": "#800020","paddingTop":20,"paddingBottom":20},
-                            ),
-                        ],
-                    ),
-                    justify="center",
-                ),
-            ]
-
-def get_feature_cards(id):
-    return [
-                dbc.Row(
-                [
-                    dbc.Col(
-                        id = id+"-text",
-                    ),
-                ]),
-                dbc.Row(
-                    id = id,
-                    justify="center"
-                )
-            ]
-
-def get_submit_button(id):
-    return [
-                dbc.Row(
-                    [
-                        dbc.Col(
-                            html.Div(
-                                dbc.Button(
-                                    id=id,
-                                    n_clicks=0,
-                                    className="mr-1"
-                                ),
-                            id="submit-features-calc-wrapper",
-                            )
-                        ),
-                    ],
-                    style = {"paddingBottom":10}
-                ),
-            ]
-
-def get_results_card(id,err_id):
-    return [
-                dbc.Row(
-                    dbc.Col(
-                        [
-                            html.P(
-                                id=err_id,
-                                style={"color":"red","textAlign":"center"}
-                            )
-                        ],
-                        xs=12,
-                        sm=12,
-                        md=12,
-                        lg=12,
-                    ),
-                    justify="center",
-                ),
-                dbc.Row(
-                    dbc.Col(
-                        [
-                        dbc.Card(
-                            [
-                                dbc.CardBody(id=id)
-                            ],
-                            color="dark",
-                            inverse=True,
-                            style={"marginTop":10,"marginBottom":20}
-                            ),
-                        ],
-                        xs=12,
-                        sm=6,
-                        md=6,
-                        lg=3,
-                    ),
-                    justify="center",
-                ),
-            ]
-
-def get_inputed_vals(id):
-     return [
-             dbc.Row(
-                dbc.Col(
-                [
-                    dcc.Markdown(
-                         id = id
-                    )
-                ],
-                ),
-                justify="center",
-             ),
-             ]
+from .utils import lang_names
 
 
-def get_personal_visual(id):
-    return [
-                dbc.Row(
-                    dbc.Col(
-                        [
-                            dbc.Card(
-                                [
-                                    dcc.Markdown(id=id+"-explanation"),
-                                    html.Img(
-                                        id = id,
-                                        style={"height":200}
-                                    ),
-                                ],
-                                style={
-                                    "borderColor": "white",
-                                    }
-                            )
-                        ],
-                    ),
-                    justify="center",
-                ),
-            ]
-
-language_ind = {
-    0: "English",
-    1: "Español",
-    2: "Italiano"
-}
-
-def get_lang(id):
+def get_labs_indicator(_id):
+    """This is the card where user selects if he has lab values or not"""
     return [
         dbc.Row(
-            dbc.Col(
-                html.Div(
-                    dcc.Dropdown(
-                        id = id,
-                        options = [{'label': language_ind[x], 'value': x} for x in [0,1,2]],
-                        value = 0,
-                        style={'marginBottom': 10, "width":"100%"}
-                    ),
+            justify="center", style={"paddingBottom": 20},
+            children=dbc.Col(
+                xs=12, sm=12, md=6, lg=6,
+                children=dbc.Card(
+                    className="projections-general-card h-100",
+                    children=dbc.CardBody([
+                        dcc.Markdown(id=_id + "_text"),
+                        dbc.Row([
+                            dbc.Col(
+                                html.Div([
+                                    dcc.Dropdown(
+                                        id=_id,
+                                        value=0,
+                                    ),
+                                ]),
+                            ),
+                        ]),
+                    ]),
                 ),
-                xs=5,
-                sm=5,
-                md=4,
-                lg=3,
             ),
-            justify="end",
+        )
+    ]
+
+
+def get_model_desc(id):
+    """This is the big text card that has the technical details of the model"""
+    return [
+        dbc.Row(
+            justify="center",
+            children=dbc.Jumbotron(
+                id=id,
+                style={'paddingBottom': '0.5rem', 'paddingTop': '0.8rem'}
+            ),
         ),
     ]
 
-def get_page_desc(id):
+
+def get_feature_importance(_id):
+    """This is the card that houses the feature importance graph at the bottom of the page"""
     return [
         dbc.Row(
-        [
+            justify="center",
+            children=dbc.Col([
+                dbc.Card(
+                    id=_id,
+                    style={"borderColor": "#800020", "paddingTop": 20, "paddingBottom": 20},
+                ),
+            ]),
+        ),
+    ]
+
+
+def get_feature_cards(_id):
+    """Create feature card container"""
+    return [
+        # The headline that says 'insert the features below'
+        dbc.Row([
             dbc.Col(
-            [
-                dbc.Jumbotron(
-                id=id,
-                style={'paddingBottom':'0.5rem','paddingTop':'0.8rem'}
-                )
-            ]
+                id=_id + "-text",
             ),
-        ],
+        ]),
+        # The container of feature cards
+        dbc.Row(
+            id=_id,
+            justify="center"
         )
+    ]
+
+
+def get_submit_button(_id):
+    """Build submit button"""
+    return [
+        dbc.Row(
+            style={"paddingBottom": 10},
+            children=dbc.Col(
+                html.Div(
+                    id="submit-features-calc-wrapper",
+                    children=dbc.Button(
+                        id=_id,
+                        n_clicks=0,
+                        className="mr-1"
+                    ),
+                )
+            ),
+        ),
+    ]
+
+
+def get_results_card(_id, err_id):
+    """Build score card and error message text"""
+    return [
+        # The red error text below the submit button
+        dbc.Row(
+            justify="center",
+            children=dbc.Col(
+                xs=12, sm=12, md=12, lg=12,
+                children=html.P(
+                    id=err_id,
+                    style={"color": "red", "textAlign": "center"}
+                ),
+            ),
+        ),
+        # The card that shows the user's score
+        dbc.Row(
+            justify="center",
+            children=dbc.Col(
+                xs=12, sm=6, md=6, lg=3,
+                children=dbc.Card(
+                    color="dark",
+                    inverse=True,
+                    style={"marginTop": 10, "marginBottom": 20},
+                    children=dbc.CardBody(id=_id)
+                ),
+            ),
+        ),
+    ]
+
+
+def get_inputed_vals(_id):
+    """Build the text that says what values where imputed"""
+    return [
+        dbc.Row(
+            justify="center",
+            children=dbc.Col([
+                dcc.Markdown(
+                    id=_id
+                )
+            ]),
+        )
+    ]
+
+
+def get_personal_visual(_id):
+    """Builds the user's personal visual that shows feature contribution to risk score"""
+    return [
+        dbc.Row(
+            justify="center",
+            children=dbc.Col(
+                style={
+                    "borderColor": "white",
+                },
+                children=[
+                    dbc.Card([
+                        dcc.Markdown(
+                            id=_id + "-explanation"
+                        ),
+                        html.Img(
+                            id=_id,
+                            style={"height": 200}
+                        ),
+                    ])
+                ]
+            ),
+        ),
+    ]
+
+
+def get_lang(_id):
+    """Builds the language selection dropdown at the top of the page"""
+    return [
+        dbc.Row(
+            justify="end",
+            children=dbc.Col(
+                xs=5, sm=5, md=4, lg=3,
+                children=html.Div(
+                    dcc.Dropdown(
+                        id=_id,
+                        options=[{'label': lang_names[x], 'value': x} for x in range(len(lang_names))],
+                        value=0,
+                        style={'marginBottom': 10, "width": "100%"}
+                    ),
+                ),
+            ),
+        ),
+    ]
+
+
+def get_page_desc(_id):
+    """Builds the first big text card that introduces users to the page"""
+    return [
+        dbc.Row([
+            dbc.Col([
+                dbc.Jumbotron(
+                    id=_id,
+                    style={'paddingBottom': '0.5rem', 'paddingTop': '0.8rem'}
+                )
+            ]),
+        ])
     ]
