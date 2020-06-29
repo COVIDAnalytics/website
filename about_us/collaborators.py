83ed73b6f4fff195c52fd240eeb6f504bec63cc7
Travis Ziegler <travisjayday@gmail.com>
2020-06-29 15:49:39 -0500

    Revert "Added new collaborator"

--- about_us/collaborators.py
+++ about_us/collaborators.py
@@ -5,63 +5,62 @@ import yaml
 from navbar import Navbar
 from footer import Footer
 
-
 def Collaborators():
-    """Builds the AboutUs->Collaborators Page using assets/collaborators/organizations.yml"""
-
     with open("assets/collaborators/organizations.yml") as f:
         collaborators = yaml.load(f, Loader=yaml.FullLoader)
+    num_collaborators = len(collaborators)
+
+    nav = Navbar()
+    footer = Footer()
 
     def get_card(collab):
-        return dbc.Col(
-            style={"margin-bottom": "32px"},
-            xs=12, sm=6, md=4, xl=4,
-            children=dbc.Card(
-                style={"borderColor": "#800020"},
-                className="h-100 collab-card",
-                children=[
-                    dbc.CardHeader(html.H4(collab["name"]), style={"textAlign": "center"}),
-                    dbc.CardImg(
-                        src='assets/collaborators/photos/%s' % collab['photo'],
-                        top=False,
-                        style={
-                            "paddingLeft":   "20px",
-                            "paddingRight":  "20px",
-                            "paddingBottom": collab["padding"],
-                            "paddingTop":    collab["padding"],
-                        }
-                    ),
-                    dbc.CardFooter(
-                        className="h-100",
-                        children=[
-                            html.A(
-                                collab["text"],
+        return \
+            dbc.Col(
+                dbc.Card(
+                    [
+                        dbc.CardHeader(html.H4(collab["name"]),style={"textAlign": "center"}),
+                        dbc.CardImg(src='assets/collaborators/photos/%s' % collab['photo'],
+                                    top=False,
+                                    style={"paddingLeft":   "10px",
+                                           "paddingRight":  "10px",
+                                           "paddingBottom": collab["padding"],
+                                           "paddingTop":    collab["padding"],}),
+                        dbc.CardFooter(
+                            [
+                                html.A(collab["text"],
                                 href=collab["website"],
-                                className="stretched-link collab-name"
-                            ),
-                        ],
-                    )
-                ],
-            ),
-        )
-    body = dbc.Container(
-        className="page-body",
-        children=[
-            dbc.Row(
-                style={'marginBottom': 20},
-                children=[
-                    dbc.Col([
-                        html.H2("Our Collaborators"),
-                        html.P('Our models would not have been possible without the valuable data and insights \
-                                provided by our partners. Our aspiration is to develop tools that they can positively \
-                                affect the care they provide to their patients.')
-                    ])
-                ],
-            ),
-            dbc.Row([
-                get_card(collaborators[i]) for i in range(len(collaborators))
-            ]),
-        ],
-    )
+                                className="stretched-link collab-name"),
+                            ],
+                            className="h-100",
+                        )
+                    ],
+                    style={"borderColor": "#800020"},
+                    className="h-100 collab-card"
+                ),
+                style={"margin": "0 rem"},
+            )
 
-    return html.Div([Navbar(), body, Footer()], className="site")
+    body = dbc.Container(
+        [   dbc.Row(
+                    [
+                        dbc.Col(
+                        [
+                            html.H2("Our Collaborators"),
+                            html.P('Our models would not have been possible without the valuable data and insights provided \
+                                    by our partners. Our aspiration is to develop tools that they can positively affect the \
+                                    care they provide to their patients.')
+                        ]
+                        )
+                    ],
+                    style={'marginBottom': 20}
+                ),
+            dbc.Row(
+                [
+                    get_card(collaborators[i]) for i in range(num_collaborators)
+                ],
+            ),
+        ],
+       className="page-body"
+    )
+    layout = html.Div([nav, body, footer],className="site")
+    return layout
